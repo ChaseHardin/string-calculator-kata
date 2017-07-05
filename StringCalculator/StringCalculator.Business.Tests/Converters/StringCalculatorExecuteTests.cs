cbab8ee6e3fb14c16e9924bea35620ba8a3508bf
Chase Hardin <chase.hardin@Chases-MacBook-Pro-2.local>
2017-07-04 20:37:41 -0500

    Initial project setup. Add logic for empty and 1 character string.

---
+++ StringCalculator/StringCalculator.Business.Tests/Converters/StringCalculatorExecuteTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Business.Tests.Converters
+{
+    [TestClass]
+    public class StringCalculatorExecuteTests
+    {
+        private Business.Converters.StringCalculator _calculator;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _calculator = new Business.Converters.StringCalculator();
+        }
+
+        [TestMethod]
+        public void ShouldReturnZero_WhenEmptyString()
+        {
+            Assert.AreEqual(0, _calculator.Execute(""));
+        }
+
+        [TestMethod]
+        public void ShouldReturnOne_WhenStringContainsOne()
+        {
+            Assert.AreEqual(1, _calculator.Execute("1"));
+        }
+
+        [TestMethod]
+        public void ShouldReturnTwo_WhenStringContainsTwo()
+        {
+            Assert.AreEqual(2, _calculator.Execute("2"));
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.Business/Converters/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace StringCalculator.Business.Converters
+{
+    public class StringCalculator
+    {
+        public int Execute(string input)
+        {
+            return input.Length == 0 ? 0 : int.Parse(input);
+        }
+    }
+}
